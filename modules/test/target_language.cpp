4a461963c89920455774f80765e1bd184ffba00b
Chris Walker <tekuconcept@ymail.com>
2019-09-04 17:44:49 -0600

    Dev Checkpoint 201909041744

---
+++ modules/test/target_language.cpp
@@ -0,0 +1,13 @@
+/**
+ * Created by TekuConcept on September 4, 2019
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout <<
+        "-- BEGIN --\n"
+        "I will be using C/C++ with CMake\n"
+        "-- END OF LINE --\n";
+    return 0;
+}
